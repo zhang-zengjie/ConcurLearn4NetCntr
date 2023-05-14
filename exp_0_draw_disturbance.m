ea144e194f1477de1ab5787c6c5985cc85f5beb4
Zengjie Zhang <zengjie.zhang@hotmail.com>
2023-05-14 18:09:34 +0200

    Update

--- exp_0_draw_disturbance.m
+++ exp_0_draw_disturbance.m
@@ -4,7 +4,7 @@ load exp_0_data.mat
 
 hold on;
 set(0, 'DefaultFigureRenderer', 'painters')
-plot(time, d,'LineWidth',1.5,'LineStyle','-.');
+plot(time, d(1:7:end,:),'LineWidth',1.5,'LineStyle','-.');
 axis([0,time(end),0,1.200001]);
 ax = gca;
 ax.YTick = 0:0.4:1.2;
@@ -17,7 +17,7 @@ xlabel('time (s)','Interpreter','latex', 'FontSize', 11);
 x0=500;
 y0=50;
 width=500;
-height=250;
+height=180;
 
 set(gcf,'position',[x0,y0,width,height]);
 hold off;
--- exp_1_draw_disturbance_error.m
+++ exp_1_draw_disturbance_error.m
@@ -6,7 +6,7 @@ load("exp_1_data.mat", "hat_beta_conv", "hat_beta", "lng_stk");
 figure();
 hold on;
 set(0, 'DefaultFigureRenderer', 'painters')
-plot(time(1:2:end), d(:,1:2:end) - hat_beta_conv(:,1:2:end),'LineWidth',1.5,'LineStyle','-.');
+plot(time(1:2:end), d(1:7:end, 1:2:end) - hat_beta_conv(1:7:end,1:2:end),'LineWidth',1.5,'LineStyle','-.');
 axis([0,time(end),-0.60001,0.60001]);
 line([0,time(end)],[0,0],'LineWidth',1.2,'LineStyle','-.','color','k');
 ax = gca;
@@ -18,14 +18,14 @@ xlabel('time (s)','Interpreter','latex', 'FontSize', 11);
 x0=500;
 y0=50;
 width=500;
-height=250;
+height=180;
 set(gcf,'position',[x0,y0,width,height]);
 hold off;
 
 figure();
 hold on;
 set(0, 'DefaultFigureRenderer', 'painters')
-plot(time(1:2:end), d(:,1:2:end) - hat_beta(:,1:2:end),'LineWidth',1.5,'LineStyle','-.');
+plot(time(1:2:end), d(1:7:end, 1:2:end) - hat_beta(1:7:end,1:2:end),'LineWidth',1.5,'LineStyle','-.');
 axis([0,time(end),-0.60001,0.60001]);
 line([0,time(end)],[0,0],'LineWidth',1.2,'LineStyle','-.','color','k');
 ax = gca;
@@ -37,7 +37,7 @@ xlabel('time (s)','Interpreter','latex', 'FontSize', 11);
 x0=500;
 y0=50;
 width=500;
-height=250;
+height=180;
 set(gcf,'position',[x0,y0,width,height]);
 hold off;
 
@@ -57,6 +57,6 @@ xlabel('time (s)','Interpreter','latex', 'FontSize', 11);
 x0=500;
 y0=50;
 width=500;
-height=200;
+height=180;
 set(gcf,'position',[x0,y0,width,height]);
 hold off;
